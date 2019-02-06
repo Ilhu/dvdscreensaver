b780ef4d68c6f53f158afc7fb67aca0ccdc21b97
Ilhu <46649783+Ilhu@users.noreply.github.com>
2019-02-06 20:12:33 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 let dvd;
+let edgeHit = 0;
+let cornerHit = 0;
 
 function setup() {
   createCanvas(1000, 600);
@@ -14,6 +16,9 @@ function dvdFunctions() {
 function draw() {
   background(230);
   dvdFunctions();
+  textSize(30);
+  text("the square has hit the edge " + edgeHit + " times", 280, 540);
+  text("the square has hit the corner " + "(doesnt yet work)" + " times", 280, 590);
 }
 
 class Dvd {
@@ -40,10 +45,14 @@ class Dvd {
 
     if (this.x + this.sizex == width || this.x == 0) {
       this.xspeed = this.xspeed * -1;
+      edgeHit = edgeHit + 1;
+      print(edgeHit)
     }
 
     if (this.y + this.sizey == height || this.y == 0) {
       this.yspeed = this.yspeed * -1;
+      edgeHit = edgeHit + 1;
+      print(edgeHit)
     }
   }
 }
